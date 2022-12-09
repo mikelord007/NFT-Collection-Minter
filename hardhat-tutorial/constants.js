052fdfa10eb5fd341de71bea68c5ba2b03dda177
lordmike007 <manujasan23@gmail.com>
2022-12-10 00:30:16 +0530

    finished smart contracts

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x624Bef20EA04D037ab46762cd33858660d9E2282"
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
